2612453beca5333369e262c2d5d291571d5c0b06
Anish Kumar <anishbishnoi127@gmail.com>
2024-12-23 13:56:41 +0530

    fix: missing export

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 export { default as Tooltip } from './components/CustomTooltip';
+export {default as Dropdown } from './components/CustomAutocomplete';
+export {default as Input} from './components/CustomTextInput';
+export {default as Select} from './components/CustomSelect';
+export {default as Modal} from './components/CustomModal';
+export {default as Radio} from './components/CustomRadioButtonGroup';
+export {default as Drawer} from './components/CustomDrawer';
+export {default as PeoplePicker} from './components/CustomPeoplePicker';
+export {default as Label} from './components/CustomInputLabel';
