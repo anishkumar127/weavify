eb5b7f1bbbc8f54c3fb3f72022519122bfee0e4d
Anish Kumar <anishbishnoi127@gmail.com>
2025-02-20 12:17:02 +0530

    fix: reorder and consolidate exports in index.ts for better organization

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
 // import './index.css';
 export { default as Dropdown } from './components/Autocomplete';
-export { default as Avatar } from './components/Avatar';
-export { default as Button } from './components/Button';
-export { default as Drawer } from './components/Drawer';
 export { default as ChoiceDropdown } from './components/Dropdown';
+export { default as Select } from './components/Select';
+
 export { default as GroupPeoplePicker } from './components/GroupPeoplePicker';
-export { default as Label } from './components/InputLabel';
+export { default as PeoplePicker } from './components/PeoplePicker';
+
+export { default as Drawer } from './components/Drawer';
 export { default as Modal } from './components/Modal';
 export { default as OverflowSet } from './components/OverflowSet';
 export { default as OverflowSetV2 } from './components/OverflowSetV2';
-export { default as PeoplePicker } from './components/PeoplePicker';
-export { default as Radio } from './components/RadioButtonGroup';
-export { default as Select } from './components/Select';
+
 export { default as TabList } from './components/TabList';
-export { default as Input } from './components/TextInput';
 export { default as TinyTab } from './components/TinyTab';
+
+export { default as Label } from './components/InputLabel';
+export { default as Radio } from './components/RadioButtonGroup';
+export { default as Input } from './components/TextInput';
+
+export { default as Avatar } from './components/Avatar';
+export { default as Button } from './components/Button';
 export { default as Tooltip } from './components/Tooltip';
 // weavify
 
