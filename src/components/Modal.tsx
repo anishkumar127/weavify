2ded07b6075f7ce71adf969e2e14065046ed5d5e
Anish Kumar <anishbishnoi127@gmail.com>
2025-03-13 13:58:26 +0530

    fix: enhance ReusableModal component with sx prop and accessibility improvements

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Modal, Typography } from '@mui/material';
+import { Box, Button, Modal, SxProps, Theme, Typography } from '@mui/material';
 import React from 'react';
 
 interface ReusableModalProps {
@@ -7,10 +7,11 @@ interface ReusableModalProps {
   title?: string;
   content?: React.ReactNode;
   actions?: React.ReactNode;
+  sx?: SxProps<Theme>;
 }
 
-const modalStyle = {
-  position: 'absolute' as 'absolute',
+const modalStyle: SxProps<Theme> = {
+  position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
@@ -21,23 +22,39 @@ const modalStyle = {
   borderRadius: 2,
 };
 
-const ReusableModal: React.FC<ReusableModalProps> = ({ open, onClose, title, content, actions }) => {
+const ReusableModal: React.FC<ReusableModalProps> = ({ open, onClose, title, content, actions, sx }) => {
   return (
-    <Modal open={open} onClose={onClose}>
-      <Box sx={modalStyle}>
+    <Modal
+      disableAutoFocus={true}
+      disableEnforceFocus
+      open={open}
+      onClose={onClose}
+      sx={{
+        '&:focus': {
+          outline: 'none',
+        },
+        '.Mui-focusVisible': {
+          outline: 'none',
+          boxShadow: 'none',
+        },
+      }}
+    >
+      <Box sx={{ ...modalStyle, ...sx }}>
         {title && (
           <Typography variant="h2" component="h2" className="!font-semibold" gutterBottom>
             {title}
           </Typography>
         )}
-        <div>{content}</div>
-        <Box sx={{ mt: 3, display: 'flex', justifyContent: 'flex-end' }}>
-          {actions || (
-            <Button variant="contained" onClick={onClose}>
-              Close
-            </Button>
-          )}
-        </Box>
+        {content}
+        {actions && (
+          <Box sx={{ mt: 3, display: 'flex', justifyContent: 'flex-end' }}>
+            {actions || (
+              <Button variant="contained" onClick={onClose}>
+                Close
+              </Button>
+            )}
+          </Box>
+        )}
       </Box>
     </Modal>
   );
