4d322d08a93452d67b393e041daf46eb658ff27f
Anish Kumar <anishbishnoi127@gmail.com>
2025-03-14 23:04:39 +0530

    chore: update build configuration and scripts for improved efficiency

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import resolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import autoprefixer from 'autoprefixer';
-import dts from 'rollup-plugin-dts';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import tailwindcss from 'tailwindcss';
@@ -14,11 +13,11 @@ export default [
   {
     input: 'src/index.ts',
     output: [
-      {
-        file: packageJson.main,
-        format: 'cjs',
-        sourcemap: true,
-      },
+      // {
+      //   file: packageJson.main,
+      //   format: 'cjs',
+      //   sourcemap: true,
+      // },
       {
         file: packageJson.module,
         format: 'esm',
@@ -29,7 +28,10 @@ export default [
       peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
+      typescript({
+        tsconfig: './tsconfig.json',
+        noCheck: true, // Disable type checking
+      }),
       terser(),
       postcss({
         extract: true, // Extract CSS into a separate file
@@ -50,10 +52,10 @@ export default [
     ],
     external: ['react', 'react-dom'],
   },
-  {
-    input: 'src/index.ts',
-    output: [{ file: packageJson.types }],
-    plugins: [dts.default()],
-    external: [/\.css$/],
-  },
+  // {
+  //   input: 'src/index.ts',
+  //   output: [{ file: packageJson.types }],
+  //   plugins: [dts.default()],
+  //   external: [/\.css$/],
+  // },
 ];
